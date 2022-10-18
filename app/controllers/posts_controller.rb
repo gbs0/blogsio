df846d91f4386e142608f7fda31580163cedb61c
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-10-18 17:04:31 -0300

    automatted commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -53,6 +53,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content, :photo)
     end
 end
